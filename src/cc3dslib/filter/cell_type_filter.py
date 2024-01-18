be9d6c61132aa4f75c654abd9b5405477d09b39a
Leon Hillmann <l.hillmann@tue.nl>
2024-01-18 13:11:33 +0100

    Feat: Add CellType filter

---
+++ src/cc3dslib/filter/cell_type_filter.py
@@ -0,0 +1,17 @@
+from typing import Iterable
+from cc3d.core.PySteppables import SteppableBasePy
+from cc3d.cpp.CompuCell import CellG
+from .filter import Filter
+
+
+class CellTypeFilter(SteppableBasePy, Filter):
+    def __init__(self, *types):
+        super().__init__(frequency=float("inf"))
+        self.cell_types = types
+
+    def __call__(self) -> Iterable[CellG]:
+        """Return cells of the specified type(s).
+
+        Wrapper around `cell_list_by_type`.
+        """
+        return self.cell_list_by_type(self.cell_types)
