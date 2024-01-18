aed57e69f354a61b985dc9bbba94912becbd9a2f
Leon Hillmann <l.hillmann@tue.nl>
2024-01-18 11:43:24 +0100

    Refactor: Refactor `Filter` into an abstract class
    Refactor `Filter` into an abstract base class in order to enable
    inheritance. Add a default implementation of the `Element` protocol to
    the base class.

--- src/cc3dslib/filter/filter.py
+++ src/cc3dslib/filter/filter.py
@@ -1,19 +1,25 @@
-from typing import Iterable, Protocol, Generic, TypeVar, Callable
+from abc import ABC, abstractmethod
+from typing import Iterable, Generic, TypeVar, Callable
+
+
+from cc3d.core.XMLUtils import ElementCC3D
+
+from cc3dslib.simulation.element import Element
 
 
 T = TypeVar("T", covariant=True)
 TO = TypeVar("TO", covariant=True)
 
 
-class Filter(Protocol, Generic[T]):
+class Filter(ABC, Element, Generic[T]):
     """A filter is a callable that returns an iterable.
 
     Filters can be used to select a subset of cells from a simulation.
     """
 
+    @abstractmethod
     def __call__(self) -> Iterable[T]:
         """Return an iterable of cells."""
-        ...
 
     def transform(
         self, transform: Callable[[Iterable[T]], Iterable[TO]]
@@ -22,6 +28,9 @@ class Filter(Protocol, Generic[T]):
 
     def apply(self, transform: Callable[[T], TO]) -> "Filter[TO]":
         return ApplyFilter(self, transform)
+
+    def build(self) -> list[ElementCC3D]:
+        return []
 
 
 TI = TypeVar("TI", covariant=True)
