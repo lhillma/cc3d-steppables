6d8569e861f3de03ee533397c9f8daae2a2f9f7a
Leon Hillmann <l.hillmann@tue.nl>
2024-05-14 15:55:04 +0000

    Fix: PBC in com tracker
    When tracking the COM of clusters, the com_tracker plugin did not
    account for the periodic boundary conditions correctly. Fix this by
    unwrapping the positions when tracking the COM.
    Add the `wrap_box` parameter to the com_tracker, which allows the user
    to specify the simulation box manually. If specified, wrap the recorded
    COM around the specified coordinates to restore the previous behaviour.

--- src/cc3dslib/analysis/com_tracker.py
+++ src/cc3dslib/analysis/com_tracker.py
@@ -24,6 +24,7 @@ class COMTracker(SteppableBasePy, Element):
         dims: int = 2,
         chunk_size=1000,
         frequency=1,
+        wrap_box: None | tuple[float, float] | tuple[float, float, float] = None,
     ):
         super().__init__(frequency)
 
@@ -31,6 +32,14 @@ class COMTracker(SteppableBasePy, Element):
         self.dims = dims
         self.chunk_size = chunk_size
         self.filter = filter
+        if wrap_box is not None:
+            self.wrap_box = np.array(wrap_box)
+            if len(self.wrap_box) == 2:
+                self.wrap_box = np.append(wrap_box, 0)
+            if len(self.wrap_box) != 3:
+                raise ValueError("wrap_box must be a 2 or 3 element tuple.")
+        else:
+            self.wrap_box = None
 
     def start(self):
         self.steps = 0
@@ -43,24 +52,33 @@ class COMTracker(SteppableBasePy, Element):
             maxshape=(None, n_particles, self.dims),
             dtype="f",
         )
-        self.coms = np.empty((self.chunk_size, n_particles, self.dims))
+
+        self.max_compartment_size = 0
+        for cells in self.filter():
+            self.max_compartment_size = max(self.max_compartment_size, len(cells))
+
+        self.coms = np.empty((self.chunk_size, n_particles, 3))
+        self.last_coms = np.zeros((n_particles, self.max_compartment_size, 3))
 
     def step(self, _):
-        if self.steps % self.chunk_size == 0:
-            self.com_dset.resize(self.com_dset.shape[0] + self.chunk_size, axis=0)
-            self.com_dset[-self.chunk_size :, :, :] = self.coms
-
         for i, cells in enumerate(self.filter()):
-            self.coms[self.steps % self.chunk_size, i, :] = 0, 0
-            for cell in cells:
-                self.coms[self.steps % self.chunk_size, i, :] += cell.xCOM, cell.yCOM
+            new_coms = np.array([self._get_com(cell) for cell in cells])
+            cell_volumes = np.array([cell.volume for cell in cells])
+            old_coms = self.last_coms[i, : len(cells), :]
 
-            self.coms[self.steps % self.chunk_size, i, :] /= len(cells)
+            unwrapped_coms = self._unwrapped_distance(old_coms, new_coms)
+
+            self.coms[self.steps % self.chunk_size, i, :] = np.average(
+                unwrapped_coms, weights=cell_volumes, axis=0
+            )
+            self.last_coms[i, :, :] = unwrapped_coms
 
         self.steps += 1
+        if self.steps % self.chunk_size == 0:
+            self._append_coms_to_h5()
 
     def finish(self):
-        self.com_dset[-self.chunk_size :, :, :] = self.coms
+        self._append_coms_to_h5()
         self.com_dset.resize(self.steps, axis=0)
         self.file.close()
 
@@ -70,3 +88,64 @@ class COMTracker(SteppableBasePy, Element):
     def build(self) -> list[ElementCC3D]:
         com_plugin = ElementCC3D("Plugin", {"Name": "CenterOfMass"})
         return [com_plugin]
+
+    def _get_com(self, cell: CellG) -> np.ndarray:
+        """
+        Get the center of mass of a cell as numpy array.
+
+        Parameters
+        ----------
+        cell : CellG
+            The cell to get the center of mass of.
+
+        Returns
+        -------
+        np.ndarray
+            The center of mass of the cell
+        """
+        return np.array(
+            [
+                cell.xCOM,
+                cell.yCOM,
+                0 if self.dims == 2 else cell.zCOM,
+            ]
+        )
+
+    def _append_coms_to_h5(self) -> None:
+        """
+        Append the center of mass positions to the HDF5 dataset.
+        """
+        self.com_dset.resize(self.com_dset.shape[0] + self.chunk_size, axis=0)
+        if self.wrap_box is None:
+            self.com_dset[-self.chunk_size :, :, :] = self.coms[:, :, : self.dims]
+            return
+
+        self.com_dset[-self.chunk_size :, :, :] = (
+            self.coms[:, :, : self.dims] % self.wrap_box[None, None, : self.dims]
+        )
+
+    def _unwrapped_distance(self, old_coms, new_coms) -> np.ndarray:
+        """
+        Compute the unwrapped distance between two center of mass positions.
+
+        Compute the distance between two center of mass positions while taking into
+        account the periodic boundary conditions of the simulation.
+
+        Parameters
+        ----------
+        old_coms : np.ndarray
+            The old center of mass positions.
+        new_coms : np.ndarray
+            The new center of mass positions.
+
+        Returns
+        -------
+        np.ndarray
+            The unwrapped center of mass positions.
+        """
+        diff = np.empty_like(old_coms)
+        for j in range(len(diff)):
+            diff[j] = -self.invariant_distance_vector(new_coms[j], old_coms[j])
+
+        new_coms = old_coms + diff
+        return new_coms
