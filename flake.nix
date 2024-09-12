c928fbf433a13fcbd68f681f91d75524a6db4d83
Leon Hillmann <l.hillmann@tue.nl>
2024-09-12 13:50:59 +0200

    Chore: Add dev shell with Python dev packages

--- flake.nix
+++ flake.nix
@@ -10,6 +10,7 @@
 
     packages.x86_64-linux.cc3dslib = let
       pkgs = import nixpkgs { system = "x86_64-linux"; };
+      cc3d = inputs.cc3d.packages.x86_64-linux.cc3d;
     in pkgs.python311Packages.buildPythonPackage {
       pname = "cc3dslib";
       version = "0.3.0";
@@ -26,6 +27,7 @@
       dependencies = with pkgs.python311Packages; [
         numpy
         h5py
+        cc3d
       ];
 
       format = "pyproject";
@@ -76,9 +78,19 @@
           cc3d-player5-wrapper
         ];
       };
-
+      cc3dslibDev = let
+        pkgs = import nixpkgs { system = "x86_64-linux"; };
+      in self.devShells.x86_64-linux.cc3dslib.overrideAttrs (oldAttrs: {
+        buildInputs = oldAttrs.buildInputs ++ (with pkgs; [
+          pyright
+          python3Packages.virtualenv
+          python3Packages.pylsp-rope
+          python3Packages.python-lsp-black
+          python3Packages.python-lsp-server
+          python3Packages.rope
+        ]);
+      });
       default = self.devShells.x86_64-linux.cc3dslib;
     };
-
   };
 }
